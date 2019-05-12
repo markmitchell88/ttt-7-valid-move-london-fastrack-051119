371eb993f3279f7c75052a8bb69db1d575d3b59e
Mark Mitchell <mmitchell88@hotmail.co.uk>
2019-05-12 12:15:23 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,6 +12,7 @@ def position_taken?(board, index)
   taken = nil
   if(board[index] == " " || board[index] == "" || board[index] == nil)
     taken = false
-  else taken = true
+  else
+    taken = true
   end
 end
