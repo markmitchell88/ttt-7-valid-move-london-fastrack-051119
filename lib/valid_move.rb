f75df543ac1501d3fe260290139be2d5d66a1477
Mark Mitchell <mmitchell88@hotmail.co.uk>
2019-05-12 11:19:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -19,8 +19,3 @@ def position_taken?(board, index)
   else taken = true
 end
 end
-
-returns true for a valid position on an empty board (FAILED - 1)
-  returns true for a valid position on a non-empty board (FAILED - 2)
-  returns nil or false for an occupied position (FAILED - 3)
-  returns nil or false for a position that is not on the board (FAILED - 4)
