51312f1b443e7156fe7a3a526e3dc6c5e3feffdf
Mark Mitchell <mmitchell88@hotmail.co.uk>
2019-05-12 11:33:57 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,13 @@
 # code your #valid_move? method here
+def valid_move(board, index)
+  index.between(0,8)
 
+  if (!position_taken? && index.between <9)
+    return true
+  else (index.between > 8)
+  return false
+end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
